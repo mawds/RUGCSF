2dd8ab2285bdffc1863dce2776f4c35a7f43a5ad
David Mawdsley <david.mawdsley@manchester.ac.uk>
2017-10-30 10:30:58 +0000

    Show base vs ms version piecewise

--- testthreads/loadJobStats.R
+++ testthreads/loadJobStats.R
@@ -9,6 +9,7 @@ loadJobStats <- function(infile){
     gather( key = "version", value = "time", -task) %>% 
     mutate(threads = as.numeric(str_match(version, "\\.(\\d+)\\.thread")[,2])) %>% 
     mutate(RProvider = if_else(grepl("Microsoft", version), "Microsoft", "Standard")) %>% 
+    mutate(RProvider = parse_factor(RProvider, levels=c("Microsoft", "Standard"))) %>% 
     mutate(quasithread = if_else(RProvider == "Standard", 0, threads)) %>% 
     mutate(infile = basename(UQ(infile)))
   
